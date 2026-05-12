ed18660e8a8804ae0688d097bb5311e2d668d532
kokkorko <kostasmaf7@gmail.com>
2026-05-12 17:25:14 +0300

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+            char[] charArray1 = s.toCharArray();
+            char[] charArray2 = t.toCharArray();
+
+            Arrays.sort(charArray1);
+            Arrays.sort(charArray2);
+
+            String sorted1 = new String(charArray1);
+            String sorted2 = new String(charArray2);
+
+            if (!sorted1.equals(sorted2)) {
+                return false;
+            }
+            return true;
+    }
+}
