fcc2e258bff29014de74bdd0d0216215255bca29
kokkorko <kostasmaf7@gmail.com>
2026-05-13 09:51:21 +0300

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+            char[] charArray1 = s.toCharArray();
+            char[] charArray2 = t.toCharArray();
+
+            Arrays.sort(charArray1);
+            Arrays.sort(charArray2);
+
+            if(!Arrays.equals(charArray1, charArray2)) return false;
+            return true;
+    }
+}
